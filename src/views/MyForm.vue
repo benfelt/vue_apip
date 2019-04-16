2048f92efb4b5362e5e20f40a2763c92b90f4ddc
Benoît Toutain <benoit.toutain@inra.fr>
2019-04-16 11:55:35 +0200

    Passage paramètre dynamique

--- src/views/MyForm.vue
+++ src/views/MyForm.vue
@@ -6,9 +6,9 @@
         <b-col cols="10"><GreetingsInputComponent /></b-col>
         <b-col></b-col>
       </b-row>
-      <b-row>
+      <b-row v-for="title in titles" :key="title">
         <b-col></b-col>
-        <b-col cols="10"><GreetingsListComponent title="dear" /></b-col>
+        <b-col cols="10"><GreetingsListComponent :title="title" /></b-col>
         <b-col></b-col>
       </b-row>
     </b-container>
@@ -22,6 +22,11 @@ import GreetingsListComponent from '@/components/GreetingsListComponent'
 
 export default {
   name: 'MyFormView',
+  data() {
+    return {
+      titles: ['dear','mr','mrs']
+    }
+  },
   components: {
     GreetingsInputComponent,
     GreetingsListComponent
