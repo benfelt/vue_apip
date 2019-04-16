b669a26a93e06b1f75af99d969d23517545a0089
Benoît Toutain <benoit.toutain@inra.fr>
2019-04-16 10:50:23 +0200

    Affichage des données dans un tableau

--- src/components/GreetingsListComponent.vue
+++ src/components/GreetingsListComponent.vue
@@ -1,21 +1,57 @@
 <template>
     <div id="greetings-list-component">
+        <b-table 
+            striped 
+            hover 
+            :items="greetings"
+            :fields="fields"
+        >
+        </b-table>
     </div>
 </template>
 
 <script>
 
 import EventBus from '@/helpers/EventBus'
+import HttpClient from '@/helpers/HttpClient'
 
 export default {
     name: 'GreetingsListComponent',
+    data() {
+        return {
+            greetings: null,
+            fields: [
+                { key: 'id', label: 'Id', class: 'text-center' },
+                { key: 'name', label: 'Name', class: 'text-left'}
+            ]
+        }
+    },
     methods: {
         load() {
             console.log("Loading Greetings")
+
+            let http = new HttpClient('greetings')
+            
+            let filters = {
+                "page": 1,
+                "itemsPerPage": 10
+            }
+
+            http.read(filters)
+            .then(response => {
+                this.greetings = response.data['hydra:member']
+                eventBus.$emit('GreetingsListComponent::load')
+            })
+            .catch(function() {
+                console.error("In GreetingsListComponent.vue : error while loading data from API")
+            })
         }
     },
     created() {
         EventBus.$on('GreetingsInputComponent::submit',this.load)
+    },
+    mounted()  {
+        this.load()
     }
 }
 </script>
